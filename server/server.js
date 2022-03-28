e83ac897efc89cd3367aede9fe6d07abdbd61bca
sznitowski <vsznitowski@gmail.com>
2022-03-28 13:24:38 -0300

    preparing for deployment

--- server/server.js
+++ server/server.js
@@ -24,14 +24,19 @@ app.use('/api/posts', postRoutes);
     app.use(express.static('client/build'));
 } */
 
-__dirname = path.resolve();
+const __dirname = path.resolve();
+
 if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "/client/build")));
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    app.get("*", (req, res) =>
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+    );
+} else {
+    app.get("/", (req, res) => {
+        res.send("API is running..");
     });
-};
-
+}
 
 /* Deployment */
 
