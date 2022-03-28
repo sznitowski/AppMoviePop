2935d6b3afd294e0735e117c57d0d1e229bdcfe9
sznitowski <vsznitowski@gmail.com>
2022-03-28 13:02:31 -0300

    preparing for deployment

--- server/server.js
+++ server/server.js
@@ -28,7 +28,9 @@ __dirname = path.resolve();
 if(process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "/client/build")))
 
-
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+    })
 } else {
     app.get("/", (req, res) => {
         res.send("Api is running...")
