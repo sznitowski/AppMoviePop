b3e8140401af683ad60ac47b8151f07833da2272
sznitowski <vsznitowski@gmail.com>
2022-03-28 12:30:02 -0300

    preparing for deployment

--- server/config/db.js
+++ server/config/db.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose');
 
-require('dotenv').config({ path: './.env' });
-
 const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI || "mongodb+srv://admin:admin@peaker.lk8q1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {
