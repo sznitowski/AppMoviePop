e8ea27c68a01622e597ba4abb0f8d38736c41c72
sznitowski <vsznitowski@gmail.com>
2022-04-01 11:50:42 -0300

    make it better

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ app.use('/api/posts', postRoutes);
 
 /* Deployment */
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE.ENV === 'production') {
     app.use(express.static(path.join(__dirname, 'client', 'build')));
 
     app.get('*', (req, res) => {
