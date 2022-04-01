ae4c074986af83cb8bc204e4f4143741894d6758
sznitowski <vsznitowski@gmail.com>
2022-03-31 22:58:24 -0300

    preparing for deployment

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
-const cors = require('cors')
 const userRoutes = require('./routes/user.routes');
 const postRoutes = require('./routes/post.routes');
 const path = require("path");
@@ -13,7 +12,6 @@ dotenv.config();
 
 connectDB();
 app.use(express.json());
-app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/api/users', userRoutes);
