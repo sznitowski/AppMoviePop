77f0f5317da4ad9b944050e067c4ed5bec0170bd
sznitowski <vsznitowski@gmail.com>
2022-03-31 21:43:12 -0300

    preparing for deployment

--- server/server.js
+++ server/server.js
@@ -38,6 +38,6 @@ if (process.env.NODE.ENV === 'production') {
 app.use(notFound);
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, console.log(`Server is running on PORT ${PORT}`));
